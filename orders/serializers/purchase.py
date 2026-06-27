2089548436b26918c610be46e8ce1e30644e7531
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2026-06-27 22:17:51 +0100

    feat: Nest service and variation details inside PurchaseSerializer

--- orders/serializers/purchase.py
+++ orders/serializers/purchase.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 from orders.models import Purchase
 
+from orders.serializers.variations import (
+    AirtimeNetworkSerializer, DataVariationSerializer, ElectricityServiceSerializer,
+    ElectricityVariationSerializer, TVVariationSerializer, InternetVariationSerializer,
+    EducationVariationSerializer
+)
+
 class PurchaseSerializer(serializers.ModelSerializer):
+    airtime_service = AirtimeNetworkSerializer(read_only=True)
+    data_variation = DataVariationSerializer(read_only=True)
+    electricity_service = ElectricityServiceSerializer(read_only=True)
+    electricity_variation = ElectricityVariationSerializer(read_only=True)
+    tv_variation = TVVariationSerializer(read_only=True)
+    internet_variation = InternetVariationSerializer(read_only=True)
+    education_variation = EducationVariationSerializer(read_only=True)
+    
+    service_details = serializers.SerializerMethodField()
+
     class Meta:
         model = Purchase
         fields = [
@@ -9,8 +25,26 @@ class PurchaseSerializer(serializers.ModelSerializer):
             "status", "initiator", "time", "remarks",
             "airtime_service", "data_variation", "electricity_service", 
             "electricity_variation", "tv_variation", "internet_variation", 
-            "education_variation", "token", "metadata"
+            "education_variation", "token", "metadata", "service_details"
         ]
+
+    def get_service_details(self, obj):
+        if obj.purchase_type == 'airtime' and obj.airtime_service:
+            return AirtimeNetworkSerializer(obj.airtime_service, context=self.context).data
+        elif obj.purchase_type == 'data' and obj.data_variation:
+            return DataVariationSerializer(obj.data_variation, context=self.context).data
+        elif obj.purchase_type == 'electricity':
+            if obj.electricity_variation:
+                return ElectricityVariationSerializer(obj.electricity_variation, context=self.context).data
+            elif obj.electricity_service:
+                return ElectricityServiceSerializer(obj.electricity_service, context=self.context).data
+        elif obj.purchase_type == 'tv' and obj.tv_variation:
+            return TVVariationSerializer(obj.tv_variation, context=self.context).data
+        elif obj.purchase_type == 'internet' and obj.internet_variation:
+            return InternetVariationSerializer(obj.internet_variation, context=self.context).data
+        elif obj.purchase_type == 'education' and obj.education_variation:
+            return EducationVariationSerializer(obj.education_variation, context=self.context).data
+        return None
 
 class BasePurchaseRequestSerializer(serializers.Serializer):
     transaction_pin = serializers.CharField(max_length=4, write_only=True)
