82647b9cb81b58f9926f1494362d2a7378bb8271
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2026-08-08 16:03:35 +0100

    fix(deployment): resolve startup database initialization warning and missing staticfiles directory warning

--- config/settings.py
+++ config/settings.py
@@ -236,6 +236,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 WHITENOISE_MANIFEST_STRICT = False
